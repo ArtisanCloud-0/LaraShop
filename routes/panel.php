8154ea761398672f8a5eee36e6f37e43b45863df
ArtisanCloud-0 <osbusiness662@gmail.com>
2026-06-16 22:31:00 +0200

    starting coding: Add Category Domain CRUD Actions

--- routes/panel.php
+++ routes/panel.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Admin\Categories\Index;
+use App\Livewire\Admin\Categories\Create;
 
 Route::get('/panel', function () {
     return 'Welcome to control panel';
@@ -9,3 +11,8 @@ Route::get('/panel', function () {
 Route::get('/panel/login', function () {
     return 'Welcome to control panel login';
 })->name('panel.login');
+
+// Categories pages
+Route::livewire('panel/categories', Index::class)->name('panel.category'); // Show Categories and SubCategories to Manage them
+
+Route::livewire('panel/categories/create', Create::class)->name('panel.category.create'); // Add new Categories
